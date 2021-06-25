67fe74397c81bc2c85a03b70c70c0aa5a51cab56
suisit <1132542411@qq.com>
2021-06-25 23:38:06 +0800

    删除xapi

--- 基于xAPI的在线学习数据跟踪/server/src/work/work.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WorkController } from './work.controller';
-
-@Module({
-  controllers: [WorkController]
-})
-export class WorkModule {}
--- 基于xAPI的在线学习数据跟踪/web/vue.config.js
+++
@@ -1,5 +0,0 @@
-// vue.config.js
-module.exports = {
-  lintOnSave: false,
-  publicPath: './',
-}
